62cf0c6169cf5b7801fd83215f7decfc68eddcff
adarkond <akshatad1234@gmail.com>
2023-11-22 14:30:27 +0530

    First Commit

---
+++ src/main/java/com/GitConfig/GitConfigure/GitConfigureApplication.java
@@ -0,0 +1,13 @@
+package com.GitConfig.GitConfigure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitConfigureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitConfigureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GitConfig/GitConfigure/GitConfigureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GitConfig.GitConfigure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitConfigureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
